c70accc20d662bb5e1696362e4d7dc8c3ed5d70d
uzo-chukwu <israeljohnson0464@gmail.com>
2025-05-26 13:17:03 +0100

    back to work

--- Backend/bidhub/src/main/java/com/bidhub/dto/BidDTO.java
+++ Backend/bidhub/src/main/java/com/bidhub/dto/BidDTO.java
@@ -5,3 +5,10 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
+public class BidDTO {
+    private String id;
+    private String auctionItemId;
+    private String userId;
+    private double bidAmount;
+    private LocalDateTime bidTime;
+}
